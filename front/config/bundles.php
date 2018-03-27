18391bdcddd4c6943a618573c46415ca78946994
Serge HARDY <serge.hardy@zenika.com>
2018-03-27 23:14:31 +0200

    Formulaire voyage

--- front/config/bundles.php
+++ front/config/bundles.php
@@ -5,4 +5,8 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
 ];
--- front/src/Controller/DeLoreanController.php
+++ front/src/Controller/DeLoreanController.php
@@ -2,23 +2,33 @@
 
 namespace App\Controller;
 
+use DateTime;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Validator\Constraints\Date;
 
 class DeLoreanController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/",name="index")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $dt = $request->get('destination_time',0);
+        $pt = $request->get('present_time',time());
+        $ltd = $request->get('last_time_departed',time());
+
         return $this->render('delorean.dashboard.html.twig',
         [
-          'destination_time' => 0,
-          'present_time' => time(),
-          'last_time_departed'=>time()
+          'destination_time' => $dt,
+          'present_time' => $pt,
+          'last_time_departed'=>$ltd,
 
         ]
       );
@@ -62,5 +72,49 @@ class DeLoreanController extends Controller
 
         ]
       );
+    }
+
+    /**
+     * @Route("/book")
+     * @param Request $request
+     */
+    public function book(ClientInterface $client,Request $request)
+    {
+        $response = $client->request('GET','http://nginx:82/api/evenements',[
+            'headers'=>[
+                //'accept'=> 'application/ld+json'
+                'Accept'=> 'application/json'
+            ]
+            ]
+        )->getBody();
+        $evenements=json_decode($response,true);
+
+        $choices = [];
+
+        foreach ($evenements as $evenement) {
+            $choices[$evenement['nom']] = $evenement['date'];
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('evenements',ChoiceType::class,['choices'=>$choices])
+            ->add('save', SubmitType::class, array('label' => 'Réserver'))
+        ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $date= $form->getData()['evenements'];
+
+            $dt = DateTime::createFromFormat( DateTime::RFC3339,$date);
+
+            $timestamp = $dt->getTimestamp();
+            return $this->redirectToRoute('index',['destination_time'=>$timestamp]);
+        }
+
+        return $this->render('book.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
     }
 }
